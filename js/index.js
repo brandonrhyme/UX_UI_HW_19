35013d3aa15b9afc9739a8ef512ab13e1f91c9d4
brandonrhyme <64618197+brandonrhyme@users.noreply.github.com>
2022-02-03 12:30:26 -0600

    RWD navbar tweaks (hamburger)
    Mobile screens will hide navigation links and instead display hamburger menu icon.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 console.log("Your index.js file is loaded correctly!")
+
+//  Zoom Hero Content Animation 
+
 $(document).ready(function(){
     $("button").hover(function(){
         $("div.content").css("scale" , "1.3")
   });
 });
+
+//  Hamburger Menu Dropdown (Borrowed from Activity 19.1?)
+//  >> first need to apply media query for hamburger icon <<
+
+$("#toggle").on("click", function(){
+    $("#dropDown").slideToggle();
+  });  
+
+//  Fade In Skills & Work Items
+
+
+
+//  Button State Animations
