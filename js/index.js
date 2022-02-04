153aced9bf56e77b5fc38ffb5ab2b08f10e96aea
brandonrhyme <64618197+brandonrhyme@users.noreply.github.com>
2022-02-03 18:53:05 -0600

    Achieved Mobile Nav Menu Toggle

--- js/index.js
+++ js/index.js
@@ -1,27 +1,16 @@
 console.log("Your index.js file is loaded correctly!")
 
-//  Zoom Hero Content Animation 
-
-// $(document).ready(function(){
-//     $("div.content").hover(function(){
-//         var div = $("div.content");  
-//         div.animate({TransformScale: '1.3'}, "fast");
-//     },
-//     function(){
-//         var div = $("div.content");  
-//         div.animate({fontSize: '1em'}, "fast");
-//     });
-// });
-
 //  Hamburger Menu Dropdown (Borrowed from Activity 19.1?)
 //  >> first need to apply media query for hamburger icon <<
+$(document).ready(function(){
+    $("#hamburgerIcon").on("click", function(){
+        $("#slideDownMenu").slideToggle("fast");
 
-$("#toggle").on("click", function(){
-    $("#dropDown").slideToggle();
-  });  
-
+    });  
+});
 //  Fade In Skills & Work Items
 
 
 
-//  Button State Animations
+//  Button State Animations <- used CSS
+
